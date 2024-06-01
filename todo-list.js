9e771e4cefd5785a76cc46ef5f9defed0e92fcc2
Zedjinale <164517754+Zedjinale@users.noreply.github.com>
2024-06-01 15:57:35 +0400

    Add files via upload

---
+++ todo-list.js
@@ -0,0 +1,54 @@
+const todoList= [
+
+];
+
+renderTodoList();
+
+function renderTodoList () {
+
+
+
+let todoListHtml = ''; 
+
+for (let i=0; i<todoList.length; i++){
+    const todoObject =todoList[i];
+    /*const name = todoObject.name;
+    const dueDate=todoObject.dueDate;
+    */
+    const { name, dueDate}=todoObject;
+
+    const html = 
+   `     <div> ${name}</div>
+         <div> ${dueDate}</div>
+         <button class="delete-todo-button" onclick="todoList.splice(${i}, 1); 
+        renderTodoList();
+        ">delete</button>
+
+`;
+    todoListHtml += html;
+};
+
+document.querySelector('.js-todo-list')
+.innerHTML=todoListHtml;
+
+};
+
+function addTodo () {
+   const inputElement= document.querySelector('.js-name-input');
+
+   const name = inputElement.value;
+   const dateInputElement=document.querySelector('.js-due-date-input');
+   const dueDate = dateInputElement.value;
+
+     todoList.push({
+        name:name,
+        dueDate:dueDate
+      });
+
+     console.log(todoList); 
+
+     inputElement.value=''; 
+
+     renderTodoList();
+
+};   
